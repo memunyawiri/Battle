c30266de736cb5780a86a2da77480117000c9478
Mutsa <mutsa.evelyn@hotmail.com>
2017-07-21 21:00:29 +0100

    (WIP) Challenge: Switching turns & Multiplayer - implented user story 1, 2 & 3, rspec not recognizing capybara method "visit" so returning two errors

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require 'sinatra/base'
 require './lib/game'
 require './lib/player'
+require './lib/attack'
 
 class Battle < Sinatra::Base
   enable :sessions
@@ -10,7 +11,6 @@ class Battle < Sinatra::Base
   end
 
   post '/names' do
-    p params
     player_1 = Player.new(params[:player_1_name])
     player_2 = Player.new(params[:player_2_name])
     $game = Game.new(player_1, player_2)
@@ -24,9 +24,15 @@ class Battle < Sinatra::Base
 
   get '/attack' do
     @game = $game
-    @game.attack(@game.player_2)
+    Attack.run(@game.opponent_of(@game.current_turn))
     erb :attack
   end
 
+  post '/switch-turns' do
+    $game.switch_turns
+    redirect('/play')
+  end
+
+ # start the server if ruby file executed directly
   run! if app_file == $0
 end
---
+++ spec/features/attacking_spec.rb
@@ -0,0 +1,33 @@
+
+feature 'Attacking' do
+  scenario 'Attack player 2' do
+    sign_in_and_play
+    click_button 'Attack'
+    expect(page).to have_content 'Dave attacked Mittens'
+  end
+
+  scenario 'be attacked by Player 2' do
+    sign_in_and_play
+    click_button 'Attack'
+    click_button 'OK'
+    click_button 'Attack'
+    expect(page).to have_content 'Mittens attacked Dave'
+  end
+
+  scenario 'reduce Player 2 HP by 10' do
+    sign_in_and_play
+    click_button 'Attack'
+    click_button 'OK'
+    expect(page).not_to have_content 'Mittens: 60HP'
+    expect(page).to have_content 'Mittens: 50HP'
+  end
+  scenario 'reduce Player 1 HP by 10' do
+    sign_in_and_play
+    click_button 'Attack'
+    click_button 'OK'
+    click_button 'Attack'
+    click_button 'OK'
+    expect(page).not_to have_content 'Dave: 60HP'
+    expect(page).to have_content 'Dave: 50HP'
+  end
+end
