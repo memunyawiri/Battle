e856f81a9cc1425ef9329903b9843b03f772009e
Mutsa <mutsa.evelyn@hotmail.com>
2017-07-21 14:51:54 +0100

    Completed challenge Skinny Controllers: shrinked some of the repitition in Controller layer

--- app.rb
+++ app.rb
@@ -1,29 +1,30 @@
 require 'sinatra/base'
-require './lib/player'
 require './lib/game'
+require './lib/player'
 
 class Battle < Sinatra::Base
   enable :sessions
+
   get '/' do
     erb :index
   end
 
   post '/names' do
-    $player_1 = Player.new(params[:player_1_name])
-    $player_2 = Player.new(params[:player_2_name])
+    p params
+    player_1 = Player.new(params[:player_1_name])
+    player_2 = Player.new(params[:player_2_name])
+    $game = Game.new(player_1, player_2)
     redirect '/play'
   end
 
   get '/play' do
-    @player_1 = $player_1
-    @player_2 = $player_2
+    @game = $game
     erb :play
   end
 
   get '/attack' do
-    @player_1 = $player_1
-    @player_2 = $player_2
-    Game.new.attack(@player_2)
+    @game = $game
+    @game.attack(@game.player_2)
     erb :attack
   end
 
