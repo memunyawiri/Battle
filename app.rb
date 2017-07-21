7e8a74763db401c77b291db0730e64b590e7f52a
Mutsa <mutsa.evelyn@hotmail.com>
2017-07-21 13:22:48 +0100

    Compelted challenges; Implementing hit points & SRP by extracting responsibility for attacking players from Player class to Game class

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require 'sinatra/base'
 require './lib/player'
+require './lib/game'
 
 class Battle < Sinatra::Base
   enable :sessions
@@ -14,14 +15,15 @@ class Battle < Sinatra::Base
   end
 
   get '/play' do
-    @player_1_name = $player_1.name
-    @player_2_name = $player_2.name
+    @player_1 = $player_1
+    @player_2 = $player_2
     erb :play
   end
 
   get '/attack' do
-    @player_1_name = $player_1.name
-    @player_2_name = $player_2.name
+    @player_1 = $player_1
+    @player_2 = $player_2
+    Game.new.attack(@player_2)
     erb :attack
   end
 
